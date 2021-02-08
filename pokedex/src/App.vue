b35fd85c173f361b3a3c9a4af3fbf5a8bdb5d23b
LiviaSibi <livia.sibinelli@gmail.com>
2021-02-08 13:57:02 -0300

    pokedex

---
+++ pokedex/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="pokedex">
+    <Pokedex />
+  </div>
+</template>
+
+<script>
+  import Pokedex from './components/Pokedex.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      Pokedex,
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  #pokedex{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 702px;
+    padding: 20px
+  }
+</style>
